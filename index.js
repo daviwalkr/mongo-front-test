5a77acd313fcb78d88e47f3dfb101dc1cbd9ea67
Walkr <davi.speck@walkr.app>
2022-08-15 11:47:13 -0300

    new commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     let domain = req.host;
     let sdIndex = req.host.indexOf(".");
@@ -9,11 +11,11 @@ app.get('/', (req, res) => {
     let subdomain = domain.slice(0, sdIndex);
 
     if(subdomain == 'aena'){
-        res.send('Hello aena')
+        res.status(200).render('indexa', { subdomain})
     }else if(subdomain == 'davi'){
-        res.send('Hello davi')
+        res.status(200).render('indexd', { subdomain})
     }else{
-        res.send('Hello anyone')
+        res.status(200).render('index', { subdomain})
     }
     
 });
