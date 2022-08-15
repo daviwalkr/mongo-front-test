b30a20486812d4b06fb85d44012d14fdf66baba9
Walkr <davi.speck@walkr.app>
2022-08-15 14:12:13 -0300

    insert database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 app.use(express.json())
@@ -7,17 +8,22 @@ app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-    // res.send('main page')
     let domain = req.host;
     let sdIndex = req.host.indexOf(".");
 
     let subdomain = domain.slice(0, sdIndex);
 
-    if(subdomain == 'aena'){
+    if (subdomain == 'aena') {
+        mongoose.connect(process.env.MONGO_AENA)
+            .then(() => res.send(`connect to mongodb in ${process.env.MONGO_AENA}`))
+            .catch((e) => res.send(`Fail to connect ${e}`))
         res.status(200).send(`${subdomain}`)
-    }else if(subdomain == 'davi'){
+    } else if (subdomain == 'davi') {
+        mongoose.connect(process.env.MONGO_DAVI)
+            .then(() => res.send(`connect to mongodb in ${process.env.MONGO_DAVI}`))
+            .catch((e) => res.send(`Fail to connect ${e}`))
         res.status(200).send(`${subdomain}`)
-    }else{
+    } else {
         res.status(200).send(`${subdomain}`)
     }
 });
@@ -28,12 +34,12 @@ app.get('/check/sub', (req, res) => {
 
     let subdomain = domain.slice(0, sdIndex);
 
-    if(subdomain == 'aena'){
-        res.status(200).render('indexa', { subdomain})
-    }else if(subdomain == 'davi'){
-        res.status(200).render('indexd', { subdomain})
-    }else{
-        res.status(200).render('index', { subdomain})
+    if (subdomain == 'aena') {
+        res.status(200).render('indexa', { subdomain })
+    } else if (subdomain == 'davi') {
+        res.status(200).render('indexd', { subdomain })
+    } else {
+        res.status(200).render('index', { subdomain })
     }
 });
 
