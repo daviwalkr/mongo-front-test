ea617528e1d51281d975a8832a1a3fdda9f5cc56
Walkr <davi.speck@walkr.app>
2022-08-15 13:35:05 -0300

    change route

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-    res.send('main page')
+    // res.send('main page')
+    let domain = req.host;
+    let sdIndex = req.host.indexOf(".");
+
+    let subdomain = domain.slice(0, sdIndex);
+
+    if(subdomain == 'aena'){
+        res.status(200).render('indexa', { subdomain})
+    }else if(subdomain == 'davi'){
+        res.status(200).render('indexd', { subdomain})
+    }else{
+        res.status(200).render('index', { subdomain})
+    }
 });
 
 app.get('/check/sub', (req, res) => {
